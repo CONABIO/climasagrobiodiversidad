791687294fce048dc7b808986e35cbf360357b80
Juan <j.escalona_s@hotmail.com>
2022-08-03 21:45:07 +0000

    Adding dropdown selector

--- test.py
+++ test.py
@@ -2,16 +2,16 @@ import dash_leaflet as dl
 import dash_leaflet.express as dlx
 import pandas as pd
 from dash_extensions.javascript import assign
-from dash import Dash, html
+from dash import Dash, html, dcc, Input, Output
 
 colorscale = ['red', 'yellow', 'green', 'blue', 'purple']  # paleta de colores
 chroma = "https://cdnjs.cloudflare.com/ajax/libs/chroma-js/2.1.0/chroma.min.js"  # libreria js para colores 
 color_prop = 'altitudmapa' #variable para determinar color 
 
 df = pd.read_csv("maices.csv")  
-df = df[df['especievalida'] == 'Zea mays mays Comiteco']  # seleccion de un taxon
-df = df[['latitud', 'longitud', 'estadomapa', 'localidad', color_prop]]  # seleccion de variables
-dicts = df.to_dict('rows')
+df1 = df[df['especievalida'] == 'Zea mays mays Comiteco']  # seleccion de un taxon
+df1 = df1[['latitud', 'longitud', 'estadomapa', 'localidad', color_prop]]  # seleccion de variables
+dicts = df1.to_dict('rows')
 for item in dicts:
     item["tooltip"] = "{} ({:.1f})".format(item['localidad'], item[color_prop])  
 geojson = dlx.dicts_to_geojson(dicts, lat = 'latitud', lon="longitud")  # conversion a geojson
@@ -37,8 +37,19 @@ geojson = dl.GeoJSON(data=geobuf, id="geojson", format="geobuf",
 # Creacion de app 
 app = Dash(external_scripts=[chroma], prevent_initial_callbacks=True)
 app.layout = html.Div([
+    html.H1('Maices en México', style={'textAlign': 'center', 'color': '#1A1A1A'}),
     dl.Map([dl.TileLayer(), geojson, colorbar]),
+    dcc.Dropdown(df.especievalida.unique(), id='pandas-dropdown-2', placeholder= 'Selecciona taxon', multi=True),
+    html.Div(id='pandas-output-container-2')
 ], style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block", "position": "relative"})
+
+
+@app.callback(
+    Output('pandas-output-container-2', 'children'),
+    Input('pandas-dropdown-2', 'value')
+)
+def update_output(value):
+    return f'Seleccionaste el taxon {value}'
 
 if __name__ == '__main__':
     app.run_server()
