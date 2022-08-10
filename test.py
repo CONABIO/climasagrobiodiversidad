b75ea77ee085cdf67c1db0b3190d9984a60980ec
Juan <j.escalona_s@hotmail.com>
2022-08-10 18:09:15 +0000

    Adding real time query

--- test.py
+++ test.py
@@ -3,46 +3,54 @@ import dash_leaflet.express as dlx
 import pandas as pd
 from dash_extensions.javascript import assign
 from dash import Dash, html, dcc, Input, Output
+import requests
+import urllib3
+urllib3.disable_warnings()
 
-colorscale = ['red', 'yellow', 'green', 'blue', 'purple']  # paleta de colores
-chroma = "https://cdnjs.cloudflare.com/ajax/libs/chroma-js/2.1.0/chroma.min.js"  # libreria js para colores 
-color_prop = 'altitudmapa' #variable para determinar color 
+#maices = pd.read_csv("maices.csv")
 
-df = pd.read_csv("maices.csv")  
-df1 = df[df['especievalida'] == 'Zea mays mays Comiteco']  # seleccion de un taxon
-df1 = df1[['latitud', 'longitud', 'estadomapa', 'localidad', color_prop]]  # seleccion de variables
-dicts = df1.to_dict('rows')
-for item in dicts:
-    item["tooltip"] = "{} ({:.1f})".format(item['localidad'], item[color_prop])  
-geojson = dlx.dicts_to_geojson(dicts, lat = 'latitud', lon="longitud")  # conversion a geojson
-geobuf = dlx.geojson_to_geobuf(geojson)  # conversion a  geobuf
-# Creacion barra de color
-vmax = df[color_prop].max()
-colorbar = dl.Colorbar(colorscale=colorscale, width=20, height=150, min=0, max=vmax, unit='m')
-# Geojson rendering logic, must be JavaScript as it is executed in clientside.
-point_to_layer = assign("""function(feature, latlng, context){
-    const {min, max, colorscale, circleOptions, colorProp} = context.props.hideout;
-    const csc = chroma.scale(colorscale).domain([min, max]);  // chroma lib to construct colorscale
-    circleOptions.fillColor = csc(feature.properties[colorProp]);  // set color based on color prop.
-    return L.circleMarker(latlng, circleOptions);  // sender a simple circle marker.
-}""")
-# Creacion geojson.
-geojson = dl.GeoJSON(data=geobuf, id="geojson", format="geobuf",
-                     zoomToBounds=True,  # when true, zooms to bounds when data changes
-                     options=dict(pointToLayer=point_to_layer),  # how to draw points
-                     superClusterOptions=dict(radius=50),   # adjust cluster size
-                     hideout=dict(colorProp=color_prop, circleOptions=dict(fillOpacity=1, stroke=False, radius=5),
-                                  min=0, max=vmax, colorscale=colorscale))
+my_query= """{
+  taxons(pagination:{limit:200} search:{field:estatus value:"aceptado" operator:eq}){
+    taxon
+    taxon_id
+    categoria
+    estatus
+  }
+}
+"""
+url="https://maices-siagro.conabio.gob.mx/api/graphql"
+statusCode = 200
 
-# Creacion de app 
-app = Dash(external_scripts=[chroma], prevent_initial_callbacks=True)
+def run_query(uri, query, statusCode):
+    request = requests.post(uri, json={'query': query}, verify=False)
+    if request.status_code == statusCode:
+        return request.json()
+    else:
+        raise Exception(f"Unexpected status code returned: {request.status_code}")
+
+
+result = run_query(url, my_query, statusCode)
+
+taxons = []
+for i in range (len(result['data']['taxons'])):
+    taxons.append(result['data']['taxons'][i])
+
+df_taxons=pd.DataFrame.from_dict(taxons)
+
+import plotly.express as px
+
+#fig = px.scatter_mapbox(maices, lat="latitud", lon="longitud", hover_name="especievalida", hover_data={'latitud':False, 'longitud':False, 'localidad':True}, color = "altitudmapa",
+#                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+#fig.update_layout(mapbox_style="open-street-map")
+#fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+app = Dash(__name__)
 app.layout = html.Div([
     html.H1('Maices en México', style={'textAlign': 'center', 'color': '#1A1A1A'}),
-    dl.Map([dl.TileLayer(), geojson, colorbar]),
-    dcc.Dropdown(df.especievalida.unique(), id='pandas-dropdown-2', placeholder= 'Selecciona taxon', multi=True),
+    dcc.Graph(id='mapa'),
+    dcc.Dropdown(df_taxons['taxon'].unique(), id='pandas-dropdown-2', placeholder='Selecciona un taxon'),
     html.Div(id='pandas-output-container-2')
-], style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block", "position": "relative"})
-
+])
 
 @app.callback(
     Output('pandas-output-container-2', 'children'),
@@ -51,5 +59,32 @@ app.layout = html.Div([
 def update_output(value):
     return f'Seleccionaste el taxon {value}'
 
-if __name__ == '__main__':
-    app.run_server()
+@app.callback(
+    Output(component_id='mapa', component_property='figure'),
+    [Input(component_id='pandas-dropdown-2', component_property='value')]
+)
+
+def update_map(column_chosen):
+    try:
+        taxon_id=df_taxons.loc[df_taxons['taxon']== column_chosen, 'taxon_id'].values[0]
+        new_query = '{\n  taxons(pagination:{limit:500} search:{field:taxon_id value:"%'+ taxon_id + '%" operator:iLike}){\n    taxon_id\n    taxon\n    registroConnection(pagination:{first:9000}){\n      edges{\n        node{\n          id\n          sitio{\n            latitud\n            longitud\n            estado\n            municipio\n            localidad\n          }\n        }\n      }\n    }\n  }\n}'
+        result= run_query(url, new_query, statusCode)
+    except: 
+        column_chosen = 'Zea mays subsp. mays raza Blando de Sonora'
+        taxon_id=df_taxons.loc[df_taxons['taxon']== column_chosen, 'taxon_id'].values[0]
+        new_query = '{\n  taxons(pagination:{limit:500} search:{field:taxon_id value:"%'+ taxon_id + '%" operator:iLike}){\n    taxon_id\n    taxon\n    registroConnection(pagination:{first:9000}){\n      edges{\n        node{\n          id\n          sitio{\n            latitud\n            longitud\n            estado\n            municipio\n            localidad\n          }\n        }\n      }\n    }\n  }\n}'
+        result= run_query(url, new_query, statusCode)    
+    complete_dict=[]
+    for i in range (len(result['data']['taxons'][0]['registroConnection']['edges'])):
+        sitio = result['data']['taxons'][0]['registroConnection']['edges'][i]['node']['sitio']
+        complete_dict.append(sitio)
+    df = pd.DataFrame.from_dict(complete_dict)
+    df = df.dropna()
+    df['especievalida'] = column_chosen
+    fig = px.scatter_mapbox(df, lat="latitud", lon="longitud", hover_name="especievalida", hover_data={'latitud':False, 'longitud':False, 'estado': True, 'municipio':True},
+                            zoom=4, height=500)
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
+app.run_server(debug=True, use_reloader=False) 
